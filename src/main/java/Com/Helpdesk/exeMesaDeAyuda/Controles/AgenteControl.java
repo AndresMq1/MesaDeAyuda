f5558db0466320a8421d5fcb2f9c81f817270964
AndresMq1 <quitoraandres4@gmail.com>
2025-09-04 22:53:35 -0500

    Controles funcionales

--- src/main/java/Com/Helpdesk/exeMesaDeAyuda/Controles/AgenteControl.java
+++ src/main/java/Com/Helpdesk/exeMesaDeAyuda/Controles/AgenteControl.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AgenteControl {
 
-    @GetMapping("/listar")
-    public String listar(){
 
-        return "listar";
-    }
 
 }
---
+++ src/main/java/Com/Helpdesk/exeMesaDeAyuda/Controles/UsuarioControl.java
@@ -0,0 +1,55 @@
+package Com.Helpdesk.exeMesaDeAyuda.Controles;
+
+import Com.Helpdesk.exeMesaDeAyuda.Servicios.UsuarioServicio;
+import Com.Helpdesk.exeMesaDeAyuda.dto.UsuariosDTO;
+import Com.Helpdesk.exeMesaDeAyuda.entidades.Usuarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/Usuarios")
+public class UsuarioControl {
+
+    @Autowired
+    UsuarioServicio usuarioServicio;
+
+    @GetMapping("/listar")
+    public String listar(Model model){
+        List<UsuariosDTO> listarUsuario = usuarioServicio.getAllUsuarios();
+        model.addAttribute("users",listarUsuario);
+        return "lista";
+    }
+    @PostMapping("/crear")
+    public String crear(@ModelAttribute UsuariosDTO usuariosDTO){
+        usuarioServicio.createUsuario(usuariosDTO);
+        return "redirect:/Usuarios/listar";
+    }
+
+    @GetMapping("/crear")
+    public String crear(Model model){
+        model.addAttribute("usuario", new UsuariosDTO());
+        return "form";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String  editar(@PathVariable Long id, Model model){
+        UsuariosDTO usuariosDTO = usuarioServicio.getUsuarioById(id);
+        model.addAttribute("usuario", usuariosDTO);
+        return "formU";
+    }
+    @PostMapping("/editar")
+    public String actualizar( @ModelAttribute UsuariosDTO usuariosDTO){
+        usuarioServicio.updateUsuario(usuariosDTO.getIdUsuario(), usuariosDTO);
+        return "redirect:/Usuarios/listar";
+    }
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable Long id){
+        usuarioServicio.deleteUsuario(id);
+        return "redirect:/Usuarios/listar";
+    }
+
+}
